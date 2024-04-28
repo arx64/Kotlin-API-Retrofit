db169708ad15f0f66644b407ee4c2f546e7cdbb0
arx64 <eror404eror@hackermail.com>
2024-04-28 21:42:59 +0700

    first commit

---
+++ app/src/main/java/com/example/kotlinapi/retrofit/ApiEndpoint.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinapi.retrofit
+
+import com.example.kotlinapi.MainModel
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface ApiEndpoint {
+
+    @GET("photos")
+    fun getPhotos(): Call<List<MainModel>>
+}
